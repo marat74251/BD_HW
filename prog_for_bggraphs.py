071e9fa2f18840b8a73b04dd56a855ca6cf4bfcc
marat74251 <ahmm20051@gmail.com>
2025-05-27 12:44:55 +0500

    Update prog_for_bggraphs.py

--- prog_for_bggraphs.py
+++ prog_for_bggraphs.py
@@ -2,11 +2,13 @@ import pandas as pd
 import psycopg2
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.stats import f_oneway
+from scipy.stats import ttest_ind
 
 conn = psycopg2.connect(
     dbname="postgres",
     user="postgres",
-    password="...",
+    password="utyz@2005",
     host="localhost",
     port="5432"
 )
@@ -29,15 +31,57 @@ plt.pie(df['revenue'], labels=df['category_name'], autopct='%1.1f%%')
 plt.title('Распределение выручки по категориям')
 plt.show()
 
+# test 1
+
+plt.figure(figsize=(10, 6))
+sns.scatterplot(data=df, x='order_count', y='revenue', hue='category_name', s=100)
+plt.title("Зависимость выручки от количества заказов")
+plt.xlabel("Количество заказов")
+plt.ylabel("Общая выручка")
+plt.legend(title="Категория")
+plt.show()
+correlation = df['order_count'].corr(df['revenue'])
+print(f"Корреляция между заказами и выручкой: {correlation:.2f}")
+
+# test 2
+
 plt.figure(figsize=(10, 6))
 sns.barplot(x='category_name', y='order_count', data=df)
 plt.title('Количество заказов по категориям')
 plt.xticks(rotation=45)
 plt.show()
 
-plt.figure(figsize=(8, 6))
-sns.scatterplot(x='order_count', y='revenue', hue='category_name', data=df, s=100)
-plt.title('Зависимость выручки от количества заказов')
-plt.xlabel('Количество заказов')
-plt.ylabel('Выручка')
+df['avg_check'] = df['revenue'] / df['order_count']
+
+categories_avg_checks = []
+for category in df['category_name']:
+    avg_check_for_category = df.loc[df['category_name'] == category]['avg_check']
+    categories_avg_checks.append(avg_check_for_category.values)
+
+stat, p_val = f_oneway(*categories_avg_checks)
+if p_val < 0.05:
+    print("Есть значительные различия между средним чеком различных категорий.")
+else:
+    print("Различия между средним чеком незначительны.")
+
+plt.figure(figsize=(10, 6))
+sns.boxplot(data=df, x='category_name', y='avg_check')
+plt.title("Средний чек по категориям")
+plt.xticks(rotation=45)
 plt.show()
+
+# test 3
+
+bins = [0, df['order_count'].quantile(0.33), df['order_count'].quantile(0.66), float('inf')]
+labels = ['Низкая активность', 'Средняя активность', 'Высокая активность']
+df['activity_level'] = pd.cut(df['order_count'], bins=bins, labels=labels)
+grouped_data = df.groupby('activity_level').agg({'avg_check': 'mean'})
+print(grouped_data)
+low_activity_avg_check = df.query("activity_level == 'Низкая активность'")['avg_check']
+high_activity_avg_check = df.query("activity_level == 'Высокая активность'")['avg_check']
+
+t_stat, p_val = ttest_ind(low_activity_avg_check, high_activity_avg_check)
+if p_val < 0.05:
+    print("Клиенты с высоким уровнем активности имеют значительно больший средний чек.")
+else:
+    print("Нет существенной разницы в среднем чеке между клиентами разной активности.")
